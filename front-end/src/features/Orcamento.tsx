4b8ec45977be6bdf7e5a2fc79e68b3b94563e944
MuriloRocha-01 <murilorochadelima.10@gmail.com>
2026-03-13 16:03:10 -0300

    style: page orcamento em andamento

--- front-end/src/features/Orcamento.tsx
+++ front-end/src/features/Orcamento.tsx
@@ -2,7 +2,26 @@
 function Orcamento(){
     return(
         <div>
-            Ola
+            <form>
+                <label>Digite o nome da empresa: </label>
+                    <input 
+                    placeholder="Nome da empresa..."
+                    ></input>
+                <label>Digite o telefone: </label>
+                    <input 
+                    placeholder="Telefone..."
+                    ></input>
+                <label>Digite o Produto:</label>
+                    <input
+                    placeholder="Nome do produto..."
+                    ></input>
+                <label>Digite a quatidade: </label>
+                    <input
+                    placeholder="Quantidade..."
+                    ></input>
+            </form>
+
+            <button>Receber orçamento</button>
         </div>
     )
 }
